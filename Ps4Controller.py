035b9d9c50b80706638f45817ecad81b3545a605
niklasforestman <49684131+niklasforestman@users.noreply.github.com>
2021-04-26 19:50:48 +0200

    Add files via upload

--- Ps4Controller.py
+++ Ps4Controller.py
@@ -1,38 +1,46 @@
 from pyPS4Controller.controller import Controller
+import time
 
+
+
+throttle = 0
+brake = 0
+steering = 0
 
 class MyController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-    def on_x_press(self):
-       print("Hello world")
-
-    def on_x_release(self):
-       print("Goodbye world")
-       print(throttle_max)
-       print(throttle_min)
-
-    def on_R3_up(self, value):
-        throttle = value
+    def on_R3_down(self, value):
+        throttle = 0.5*(0.0000305185*value + 1)
         print(throttle)
 
     def on_R3_up(self, value):
-        throttle_max = 0
-        throttle_min = 0
-        throttle = value
+    
+        throttle = 0.5*(0.0000305185*value + 1)
         print(throttle)
-        if throttle_max <throttle:
-            throttle_max = throttle
-            
-        if throttle_min > throttle:
-            throttle_min = throttle
+
+    def on_L3_right(self, value):
+        steering = 0.0000305185*value 
+        print(steering)
+
+    def on_L3_left(self, value):
+        steering = 0.0000305185*value
+        print(steering)
+
+    def on_R3_left(self, value):
+        brake = 0.5*(0.0000305185*value + 1)
+        print(brake)
+
+    def on_R3_right(self, value):
+    
+        brake = 0.5*(0.0000305185*value + 1)
+        print(brake)
+
         
-
-
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
-controller.listen(timeout=60)
 
+controller.listen(timeout=60)
