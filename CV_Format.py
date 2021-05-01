9edd2f9f3adb447531f99aae3c3d5241a872c6ab
Maximilian Kleine <38472561+DoktorlHouse@users.noreply.github.com>
2021-05-01 21:35:02 +0200

    Update CV_Format.py

--- CV_Format.py
+++ CV_Format.py
@@ -40,28 +40,32 @@ def save_images(path):
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=0))
     i = 1  # Laufvariable für die Dateibenennung
-    cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    #cap = cv2.VideoCapture( gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)480, height=(int)360,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink")
     if cap.isOpened():
         window_handle = cv2.namedWindow("CSI Camera", cv2.WINDOW_AUTOSIZE)
         # Window
 
-        while cv2.getWindowProperty("CSI Camera", 0) >= 0:
+        while True:
             return_key, frame = cap.read()
             cv2.imshow("CSI Camera", frame)
-            cv2.imwrite(os.path.join(path, str(i), '.jpg'), frame)
-            k = cv2.waitKey(1)
+            pfad = str('/home/maxi/STUD_ING_DATA/' + str(i) + '.jpg')
+            cv2.imwrite(pfad, frame)
+            k = cv2.waitKey(10)
+            #print(cap)
+            #print(frame)
             if k % 256 == 27:  # ESC pressed
                 print("Escape hit, closing...")
                 break
             i = i + 1  # Die Laufvariable wird für die Dateibenennung verwendet
             print(f"Das {i}-te Bild wurde gerade gespeichert")
 
-            cap.release()
-            cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
     else:
         print("Unable to open camera")
 
 
 if __name__ == "__main__":
-    path = '/home/DATA'
+    path = '/home/STUD_ING_DATA/'
     save_images(path)
