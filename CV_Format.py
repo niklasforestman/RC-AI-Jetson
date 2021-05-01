0d66422210587dd510d540296b76e74b709d9928
Maximilian Kleine <38472561+DoktorlHouse@users.noreply.github.com>
2021-05-01 21:37:27 +0200

    Update CV_Format.py

--- CV_Format.py
+++ CV_Format.py
@@ -8,34 +8,6 @@ import os
 # display_width and display_height determine the size of the window on the screen
 
 
-def gstreamer_pipeline(
-        capture_width=1280,
-        capture_height=720,
-        display_width=1280,
-        display_height=720,
-        framerate=60,
-        flip_method=0,
-):
-    return (
-            "nvarguscamerasrc ! "
-            "video/x-raw(memory:NVMM), "
-            "width=(int)%d, height=(int)%d, "
-            "format=(string)NV12, framerate=(fraction)%d/1 ! "
-            "nvvidconv flip-method=%d ! "
-            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-            "videoconvert ! "
-            "video/x-raw, format=(string)BGR ! appsink"
-            % (
-                capture_width,
-                capture_height,
-                framerate,
-                flip_method,
-                display_width,
-                display_height,
-            )
-    )
-
-
 def save_images(path):
     # To flip the image, modify the flip_method parameter (0 and 2 are the most common)
     print(gstreamer_pipeline(flip_method=0))
