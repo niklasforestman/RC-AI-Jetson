c3fa12afc7f36da9a9cb365cd4ea99eba911170d
Niklas Waldmann <niklaswaldmann@gmx.de>
2021-05-09 12:52:39 +0200

    Funktion für Controller Output Test

--- Ps4Controller.py
+++ Ps4Controller.py
@@ -15,32 +15,39 @@ class MyController(Controller):
     def on_R3_down(self, value):
         throttle = 0.5*(0.0000305185*value + 1)
         print(throttle)
+        return throttle
 
     def on_R3_up(self, value):
     
         throttle = 0.5*(0.0000305185*value + 1)
         print(throttle)
-
+        return throttle
     def on_L3_right(self, value):
         steering = 0.0000305185*value 
         print(steering)
-
+        return steering
     def on_L3_left(self, value):
         steering = 0.0000305185*value
         print(steering)
-
+        return steering
     def on_R3_left(self, value):
         brake = 0.5*(0.0000305185*value + 1)
         print(brake)
-
+        return brake
     def on_R3_right(self, value):
     
         brake = 0.5*(0.0000305185*value + 1)
         print(brake)
+        return brake
+    def output(self):
 
-        
+        brake= self.on_R3_left(self.event.value) + self.on_R3_right(self.event.value)
+        throttle = self.on_R3_down(self.event.value) + self.on_R3_up(self.event.value)
+        steering =  self.on_L3_left(self.event.value) + self.on_L3_right(self.event.value)
 
+        return brake, throttle, steering
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
 
 controller.listen(timeout=60)
+print(controller.output())
